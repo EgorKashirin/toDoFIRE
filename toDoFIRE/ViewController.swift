2adf113b808c4a0eac712960f0c0640ae821315c
Kashirin Yegor <egor4815162342@yandex.ru>
2019-11-06 21:34:26 +0300

    Initial Commit

---
+++ toDoFIRE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toDoFIRE
+//
+//  Created by Kashirin Egor on 06.11.2019.
+//  Copyright © 2019 Kashirin Egor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
